01cec38fea0aa87938ceb3150b6073a1cd5ab59e
G00420811@atu.ie <G00420811@atu.ie>
2023-10-24 10:54:53 +0100

    Added some initial files

---
+++ src/main/java/ie/atu/Student.java
@@ -0,0 +1,16 @@
+package ie.atu;
+
+import java.util.Scanner;
+public class Student {
+    public Student() {
+        this.studentName = "";
+        this.studentEmail = "";
+        this.studentCourse ="";
+    }
+
+    public Student(String studentName, String studentEmail, String studentCourse){
+        this.studentName= Name;
+        this.studentEmail= Email;
+        this.student= Course;
+    }
+}
